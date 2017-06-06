16ca3a15ebfe9858fab93942b8e608573e140a8e
jgarciadhe <jgarcia@canaldhe.com>
2017-06-06 16:31:23 -0500

    Add files via upload

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(0);
-$servidor="192.185.104.220:2082"; // Nuestro servidor 
+$servidor="canaldhe.com:2083"; // Nuestro servidor 
 $database="elux23_programacion"; // Nombre de nuestra base de datos 
 $password="Vk.4AVd}K-"; // Nuestro password 
 $usuario="elux23_dhepgm"; // Nombre de nuestro usuario 
